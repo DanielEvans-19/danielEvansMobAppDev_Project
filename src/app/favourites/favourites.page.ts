bd1f0985474ea3eccae8d9d213b699937cfc9a62
Daniel Evans <danieldotevans@gmail.com>
2025-12-30 19:38:16 +0000

    Add functionality for displaying favourites

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -5,6 +5,8 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol, IonFab, I
 import { addIcons } from 'ionicons';
 import { heart, settings, cog, home } from 'ionicons/icons';
 import { FavouritesService } from '../services/favourites.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-favourites',
@@ -15,15 +17,54 @@ import { FavouritesService } from '../services/favourites.service';
 })
 export class FavouritesPage implements OnInit {
 
-  private favouritesArray: string[] = [];
+  favouritesArray: string[] = [];
+  favouritesObjArray: any[] = [];
 
-  constructor(private favourites: FavouritesService) { 
+  favTitle: string = "";
+  titlesList: string[] = [];
+
+  imageSrcList: string[] = [];
+  favImageSource: string = "";
+
+  constructor(private favourites: FavouritesService, private http: HttpClient) { 
     addIcons({home,heart,cog,settings});
    }
 
   async ngOnInit() {
     this.favouritesArray = await this.favourites.getAll();
-    console.log(this.favouritesArray);
+    
+    for (var i = 0; i < this.favouritesArray.length; i++) {
+      this.getFavourites(this.favouritesArray[i]);
+    }
+
+    console.log(this.imageSrcList);
+    console.log(this.titlesList);
+
   }
 
+  get(Id: string): Observable<any> {
+    return this.http.get("https://api.spoonacular.com/recipes/" + Id + "/information?apiKey=70759a4f7911402abcc53d3c51d3b759");
+  }
+
+  // Iterate through favouritesArray and get recipe information by ID.
+  getFavourites(Id: string) {
+    this.get(Id).subscribe(
+    {
+      next: (results) => {
+        console.log(results);
+        this.favouritesObjArray = results.results;
+        this.favTitle = results.title;
+        this.favImageSource = results.image;
+      },
+      error: (e) => console.error(e),
+      complete: () => {
+        console.info('complete');
+        this.titlesList.push(this.favTitle);
+        this.imageSrcList.push(this.favImageSource);
+        return this.favouritesObjArray;
+      }
+    }
+  )
+  return this.favouritesObjArray;
+}
 }
